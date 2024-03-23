24653ecf116e35a6d4ecab56a888016f2ba831c2
Lee Thomason <leethomason@gmail.com>
2024-03-23 16:08:59 -0700

    missing shell script

---
+++ package.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# writes the binaries and script files to a zippable folder
+# Useage, in the root of the project:
+# ./deploy.sh lurp_0.5.0
+
+# exit on errors
+set -e
+
+# create the folder
+mkdir $1
+
+cp *.md $1
+cp LICENSE $1
+
+# copy the binaries
+cp build/Release/lurp.exe $1
+chmod +x $1/lurp.exe
+
+# copy required script files
+mkdir $1/script
+cp -r script/* $1/script
+
+# copy game files
+mkdir $1/game
+cp -r game/* $1/game
+
